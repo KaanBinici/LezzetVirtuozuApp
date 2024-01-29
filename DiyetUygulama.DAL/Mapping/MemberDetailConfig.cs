c0fcaec3386e037cd687164a350a666974e8319d
ghost <ghost@GHOST2023>
2024-01-29 14:37:52 +0300

    ads

--- DiyetUygulama.DAL/Mapping/MemberDetailConfig.cs
+++ DiyetUygulama.DAL/Mapping/MemberDetailConfig.cs
@@ -14,12 +14,12 @@ namespace DiyetUygulama.DAL.Mapping
         public void Configure(EntityTypeBuilder<MemberDetail> builder)
         {
             builder.HasKey(x => x.MemberDetailId);
-            builder.Property(x => x.BirthDate).HasColumnType("datetime2");
+            builder.Property(x => x.BirthDate).HasColumnType("datetime2").IsRequired();
             builder.Property(x => x.Gender).HasColumnType("bit");
-            builder.Property(x => x.Height).HasColumnType("double");
-            builder.Property(x => x.Weight).HasColumnType("double");
+            builder.Property(x => x.Height).HasColumnType("double").IsRequired();
+            builder.Property(x => x.Weight).HasColumnType("double").IsRequired();
             builder.Property(x => x.FatRatio).HasColumnType("double");
-            builder.Property(x => x.IsDiabetes).HasColumnType("bit");
+            builder.Property(x => x.IsDiabetes).HasColumnType("bit").IsRequired();
             builder.Ignore(x => x.Age);
         }
     }
